f2e2f4aaac2b3ea0f6d13fcb30d6115b141e17a0
3antozz <106344848+3antozz@users.noreply.github.com>
2025-02-09 18:57:04 +0100

    Scaffold express and react project folders

---
+++ api/app.js
@@ -0,0 +1,75 @@
+const express = require('express')
+const asyncHandler = require('express-async-handler')
+const path = require('node:path')
+const cors = require('cors')
+
+
+const app = express();
+
+
+const allowedOrigins = [
+    'http://localhost:5173'
+];
+const corsOptions = {
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true,
+    optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
+app.options('*', cors((corsOptions)))
+app.set('trust proxy', 1)
+app.use(express.json())
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+
+
+app.use((req, res, next) => {
+    const error = new Error('404 Not Found')
+    error.code = 404;
+    next(error)
+})
+
+
+
+// eslint-disable-next-line no-unused-vars
+app.use((error, req, res, next) => {
+    console.log(error);
+    if (Array.isArray(error)) {
+        return res.status(error.code || 500).json({
+            errors: error,
+            code: error.code || 500
+        });
+    }
+    if (error.code === 'P2025') {
+        return res.status(400).json({
+            message: "Resource Not Found",
+            code: 400
+        });
+    }
+    if (error.code === 'P2002') {
+        return res.status(400).json({
+            errors: ['Username already taken'],
+            code: 400
+        });
+    }
+    res.status(error.code || 500).json({
+        message: error.message || 'Internal Server Error',
+        code: error.code || 500
+    });
+})
+
+
+
+
+
+app.listen(3000, () => console.log('Server Listening on port 3000'));
